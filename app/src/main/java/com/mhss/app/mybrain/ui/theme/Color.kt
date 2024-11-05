3fc6ac5f0a9ea56286df00ee697fbc395d8ce8af
Agus <aguslasp@gmail.com>
2024-11-05 05:17:37 -0300

    fix(theme colors)

--- app/src/main/java/com/mhss/app/mybrain/ui/theme/Color.kt
+++ app/src/main/java/com/mhss/app/mybrain/ui/theme/Color.kt
@@ -2,17 +2,18 @@ package com.mhss.app.mybrain.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryColor = Color(0xFF2DD1E7)
+val PrimaryColor = Color(0xFF39FF14)
 val PrimaryDarkColor = Color(0xFF00B0B0)
-val SecondaryColor = Color(0xFF5939CE)
-val DarkGray = Color(0xFF090909)
+val SecondaryColor = Color(0xFF39FF14)
+val DarkGray = Color(0xFF272727)
 
 //val SurfaceGray = Color(0xFF121212)
 val Red = Color(0xFFD53A2F)
 val Blue = Color(0xFF2965C9)
-val Green = Color(0xFF1E9651)
+val Green = Color(0xFF39FF14)
 val Orange = Color(0xFFFF9800)
 val Purple = Color(0xFF6F4CAD)
+val Black = Color(0xFF000000)
 
 val Gray = Color(0xFF7E7979)
 val LightGray = Color(0xFFECECEC)
