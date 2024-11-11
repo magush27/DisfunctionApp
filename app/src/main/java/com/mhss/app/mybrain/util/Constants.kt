5d75f7cc7e62442722d7d8c0fef198c9adc87eb4
Agus <aguslasp@gmail.com>
2024-11-11 04:22:02 -0300

    fix(constants): arreglo links del proyecto

--- app/src/main/java/com/mhss/app/mybrain/util/Constants.kt
+++ app/src/main/java/com/mhss/app/mybrain/util/Constants.kt
@@ -36,11 +36,11 @@ object Constants {
     const val FOLDER_ID = "folder_id"
 
     // lINKS
-    const val PROJECT_GITHUB_LINK = "https://github.com/mhss1/ByBrain"
-    const val PROJECT_ROADMAP_LINK = "https://github.com/users/mhss1/projects/2/"
-    const val PRIVACY_POLICY_LINK = "https://github.com/mhss1/ByBrain/blob/master/privacy-policy.md"
-    const val GITHUB_ISSUES_LINK = "https://github.com/mhss1/ByBrain/issues"
-    const val GITHUB_RELEASES_LINK = "https://github.com/mhss1/ByBrain/releases"
+    const val PROJECT_GITHUB_LINK = "https://github.com/magush27/DisfunctionApp"
+    const val PROJECT_ROADMAP_LINK = "https://github.com/magush27/DisfunctionApp"
+    const val PRIVACY_POLICY_LINK = "https://github.com/magush27/DisfunctionApp/blob/disfuncionapp/privacy-policy.md"
+    const val GITHUB_ISSUES_LINK = "https://github.com/magush27/DisfunctionApp/issues/disfuncionapp/"
+    const val GITHUB_RELEASES_LINK = "https://github.com/magush27/DisfunctionApp/releases"
 
     // Backup
     const val EXPORT_DIR = "MyBrain Export"
